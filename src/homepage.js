fd61bd9d17279a9b497661247b34cc3ea76cbdda
ealpha072 <ealpha072@gmail.com>
2020-12-27 16:40:41 +0300

    Courasel improvemnets

--- src/homepage.js
+++ src/homepage.js
@@ -95,24 +95,24 @@ function createHomepage(){
         <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
         <div class="carousel-inner">
           <div class="carousel-item active">
-            <img class="d-block w-100" src="../src/img/wp1.jpg" alt="First slide">
+            <img class="d-block w-100" src="../src/img/wp1.jpg" alt="First slide" class="img-fluid max-height:100" style="max-width: 100%;height: auto;">
             <div class="carousel-caption d-none d-md-block">
-              <h5>Fine Dining at Raymond</h5>
-              <p>Ellegant</p>
+              <h3>Fine Dining at Raymond</h3>
+              <p>Ellegance at its best</p>
             </div>
           </div>
           <div class="carousel-item">
-            <img class="d-block w-100" src="../src/img/wp2.jpg" alt="Second slide">
+            <img class="d-block w-100" src="../src/img/wp2.jpg" alt="Second slide" class="img-fluid max-height:100" style="max-width: 100%;height: auto;">
             <div class="carousel-caption d-none d-md-block">
-              <h5>Fine Dining at Raymond</h5>
-              <p>Ellegant</p>
+              <h3>Fine Dining at Raymond</h3>
+              <p>Ellegance at its best</p>
             </div>
           </div>
           <div class="carousel-item">
-            <img class="d-block w-100" src="../src/img/wp3.jpg" alt="Third slide">
+            <img class="d-block w-100" src="../src/img/wp3.jpg" alt="Third slide" class="img-fluid max-height:100" style="max-width: 100%;height: auto;">
             <div class="carousel-caption d-none d-md-block">
-              <h5>Fine Dining at Raymond</h5>
-              <p>Ellegant</p>
+              <h3>Fine Dining at Raymond</h3>
+              <p>Ellegance at its best</p>
             </div>
           </div>
         </div>
